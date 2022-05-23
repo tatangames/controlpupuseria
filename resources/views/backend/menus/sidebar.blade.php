1c0d0016962d04c34accf596eab11a945cbea8aa
jonathan moran <tatangamess@gmail.com>
2022-05-23 10:34:15 -0600

    test 10

--- resources/views/backend/menus/sidebar.blade.php
+++ resources/views/backend/menus/sidebar.blade.php
@@ -195,10 +195,7 @@
 
                 @can('seccion.administradores')
                 <li class="nav-item">
-                    <a href="{{ route('admin.permisos.index') }}" target="frameprincipal" class="nav-link">
-                        <i class="far fa-user nav-icon"></i>
-                        <p>Administradores</p>
-                    </a>
+
                 </li>
                 @endcan
 
